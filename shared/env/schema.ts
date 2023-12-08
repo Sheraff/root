dbc0dd2930b543f5656a2d2811634be09e5264da
Flo <florian.pellet@matera.eu>
2023-12-08 15:22:27 +0100

    add validation for auth secret

--- shared/env/schema.ts
+++ shared/env/schema.ts
@@ -4,7 +4,7 @@ const envSchema = object({
 	PORT: transform(string([minLength(4)]), (port) => parseInt(port, 10)),
 	DEV_PROXY_SERVER_PORT: optional(transform(string([minLength(4)]), (port) => parseInt(port, 10))),
 
-	SESSION_COOKIE_SECRET: string(),
+	SESSION_COOKIE_SECRET: string([minLength(32)]),
 
 	TWITCH_CLIENT_ID: string([minLength(1)]),
 	TWITCH_CLIENT_SECRET: string([minLength(1)]),
