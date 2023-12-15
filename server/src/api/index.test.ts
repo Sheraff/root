a35a4dc20763317b6b250dc9d4efcc90d0420ea4
Sheraff <fpellet@ensc.fr>
2023-12-15 19:14:04 +0100

    add basic test for /api routes

---
+++ server/src/api/index.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from "vitest"
+import api from "~/api"
+import fastify from "fastify"
+
+describe("api", () => {
+	const app = fastify()
+	app.register(api)
+	it("works", async () => {
+		const foo = await app.inject("/api/hello")
+		expect(foo.statusCode).toBe(200)
+		expect(await foo.json()).toEqual({ hello: "world" })
+	})
+	it("rejects unauthorized", async () => {
+		const foo = await app.inject("/api/protected")
+		expect(foo.statusCode).toBe(401)
+		expect(await foo.json()).toEqual({ error: "unauthorized" })
+	})
+})
