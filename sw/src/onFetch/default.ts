7920c28bb8afa1d219edfe1bf3c2d4a38fff472a
Sheraff <fpellet@ensc.fr>
2023-12-16 15:26:42 +0100

    SW: give up on network if it takes more than RTT

--- sw/src/onFetch/default.ts
+++ sw/src/onFetch/default.ts
@@ -2,30 +2,46 @@ import { CACHES } from "~/config"
 import indexHtml from "../../../client/index.html"
 
 function networkFirst(event: FetchEvent, request: Request, url: URL) {
-	return fetch(request)
-		.then((response) => {
+	return new Promise<Response>((resolve) => {
+		const controller = new AbortController()
+		const timeout = setTimeout(
+			() => {
+				caches.match(event.request.url, { cacheName: CACHES.assets }).then((response) => {
+					if (response) {
+						controller.abort()
+						resolve(response)
+					} else if (url.pathname === "/") {
+						controller.abort()
+						resolve(
+							new Response(indexHtml, {
+								status: 200,
+								headers: {
+									"Content-Type": "text/html; charset=utf-8",
+								},
+							}),
+						)
+					}
+				})
+			},
+			navigator.connection?.rtt !== undefined
+				? Math.min(navigator.connection.rtt + 100, 1000)
+				: 1000,
+		)
+		fetch(request, { signal: controller.signal }).then((response) => {
 			if (response.status === 200) {
+				clearTimeout(timeout)
 				const cacheResponse = response.clone()
 				caches.open(CACHES.assets).then((cache) => {
 					cache.put(event.request.url, cacheResponse)
 				})
+				resolve(response)
+			} else {
+				caches
+					.match(event.request.url, { cacheName: CACHES.assets })
+					.then((cacheResponse) => !controller.signal.aborted && resolve(cacheResponse ?? response))
 			}
-			return response
 		})
-		.catch(async (e) => {
-			const matchedResponse = await caches.match(event.request.url, { cacheName: CACHES.assets })
-			if (matchedResponse) return matchedResponse
-			console.error(new Error(`[SW] no matched response for ${event.request.url}`, { cause: e }))
-			if (url.pathname === "/") {
-				return new Response(indexHtml, {
-					status: 200,
-					headers: {
-						"Content-Type": "text/html; charset=utf-8",
-					},
-				})
-			}
-			return new Response("No connection", { status: 0 })
-		})
+	})
 }
 
 async function cacheFirst(event: FetchEvent, request: Request, url: URL) {
@@ -54,12 +70,16 @@ declare global {
 	interface WorkerNavigator {
 		connection?: {
 			downlink: number
+			rtt: number
 		}
 	}
 }
 
 export function defaultFetch(event: FetchEvent, request: Request, url: URL) {
-	if (navigator.connection?.downlink !== undefined && navigator.connection?.downlink < 0.5) {
+	if (
+		(navigator.connection?.downlink !== undefined && navigator.connection?.downlink < 0.5) ||
+		(navigator.connection?.rtt !== undefined && navigator.connection?.rtt > 1000)
+	) {
 		event.respondWith(cacheFirst(event, request, url))
 	} else {
 		event.respondWith(networkFirst(event, request, url))
