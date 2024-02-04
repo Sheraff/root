f0d50c8a0321527083d54c3131768828c7426f82
Sheraff <fpellet@ensc.fr>
2024-02-04 13:28:44 +0100

    SW: fix asset hashing & better build logs

--- worker/esbuild.ts
+++ worker/esbuild.ts
@@ -2,12 +2,17 @@
 
 import { compressBuffer } from "script/compressBuffer"
 import * as esbuild from "esbuild"
-import { readFile, readdir, writeFile } from "node:fs/promises"
+import { readFile, readdir, writeFile, rm } from "node:fs/promises"
 import { join } from "node:path"
 
 import { loadEnv } from "vite"
 import { createHash } from "node:crypto"
 import { createReadStream } from "node:fs"
+import chalk from "chalk"
+import { coloredFile } from "script/coloredFile"
+import { readableTime } from "script/readableTime"
+import { readableSize } from "script/readableSize"
+import { walkFsTree } from "script/walkFsTree"
 
 const options: esbuild.BuildOptions = {
 	entryPoints: ["src/index.ts"],
@@ -61,10 +66,22 @@ const logger: esbuild.Plugin = {
 async function compressFile(relative: string) {
 	const path = join(process.cwd(), relative)
 	const buffer = await readFile(path)
-	if (buffer.byteLength < 1501) return
+	if (buffer.byteLength < 1501) {
+		console.log(
+			chalk.gray("Skipping"),
+			coloredFile(path),
+			chalk.gray(`(too small: ${readableSize(buffer.byteLength)})`)
+		)
+		return
+	}
 	/** @type {Buffer} */
 	const compressed = await compressBuffer(buffer)
-	return writeFile(path + ".br", compressed)
+	await writeFile(path + ".br", compressed)
+	console.log(
+		chalk.gray("Compressed"),
+		coloredFile(path),
+		chalk.gray(`(${readableSize(buffer.byteLength)} -> ${readableSize(compressed.byteLength)})`)
+	)
 }
 
 async function build() {
@@ -82,30 +99,71 @@ async function build() {
 	options.define["import.meta.env.PROD"] = "true"
 	options.define["import.meta.env.SSR"] = "false"
 
-	// assets
-	const outDir = join(process.cwd(), "../dist/client")
-	const buildInclude = /\.(js|css|wasm)$/
-	const buildFiles = readdir(join(outDir, "assets")).then((files) =>
-		files.filter((f) => buildInclude.test(f)).map((f) => `/assets/${f}`)
-	)
-	const staticExclude = ["index.html"]
-	const staticFiles = readdir(outDir, { withFileTypes: true }).then((files) =>
-		files.filter((f) => f.isFile() && !staticExclude.includes(f.name)).map((f) => `/${f.name}`)
-	)
-	const files = await Promise.all(["/", buildFiles, staticFiles])
-	options.define["__CLIENT_ASSETS__"] = JSON.stringify(files.flat())
+	assets: {
+		const outDir = join(process.cwd(), "../dist/client")
+		const before = Date.now()
+		console.log(chalk.green("emitting SW assets..."))
+		const buildInclude = /\.(js|css|wasm)$/
+		const buildFiles = readdir(join(outDir, "assets")).then((files) =>
+			files.filter((f) => buildInclude.test(f)).map((f) => `/assets/${f}`)
+		)
+		const staticExclude = ["index.html"]
+		const staticFiles = readdir(outDir, { withFileTypes: true }).then((files) =>
+			files
+				.filter(
+					(f) =>
+						f.isFile() &&
+						!f.name.endsWith(".DS_Store") &&
+						!staticExclude.includes(f.name)
+				)
+				.map((f) => `/${f.name}`)
+		)
+		const f = await Promise.all(["/", buildFiles, staticFiles])
+		const files = f.flat()
+		options.define["__CLIENT_ASSETS__"] = JSON.stringify(files)
+		console.log(chalk.gray("SW cached assets list"))
+		files.forEach((f) =>
+			console.log(coloredFile(join("../dist/client", f === "/" ? "index.html" : f)))
+		)
 
-	// cache versioning
-	const hash = createHash("sha256")
-	for (let file of files.flat()) {
-		file = file === "/" ? "index.html" : file
-		createReadStream(join(outDir, file)).pipe(hash)
+		// cache versioning
+		const hash = createHash("sha256")
+		for (let file of files) {
+			file = file === "/" ? "index.html" : file
+			const stream = createReadStream(join(outDir, file))
+			stream.pipe(hash, { end: file === files[files.length - 1] })
+			await new Promise((resolve) => stream.once("end", resolve))
+		}
+		const key = hash.digest("hex")
+		options.define["__CLIENT_ASSETS_HASH__"] = JSON.stringify(key)
+		console.log(chalk.gray("SW cached assets hash"), key)
+
+		const delta = Date.now() - before
+		console.log(chalk.green(`✓ assets emitted in ${readableTime(delta)}`))
 	}
-	options.define["__CLIENT_ASSETS_HASH__"] = JSON.stringify(hash.digest("hex"))
-
-	//
-	await esbuild.build(options)
-	await compressFile("../dist/worker/sw.js")
+	build: {
+		const before = Date.now()
+		console.log(chalk.green("building service worker..."))
+		await rm("../dist/worker", { force: true, recursive: true })
+		await esbuild.build(options)
+		const delta = Date.now() - before
+		await walkFsTree("../dist/worker", (node) => {
+			if (node.stats.isDirectory()) return
+			if (node.path.endsWith(".map")) return
+			console.log(
+				coloredFile(node.path),
+				chalk.bold(chalk.gray(readableSize(node.stats.size)))
+			)
+		})
+		console.log(chalk.green(`✓ built in ${readableTime(delta)}`))
+	}
+	compress: {
+		const before = Date.now()
+		console.log(chalk.green("compressing service worker..."))
+		await compressFile("../dist/worker/sw.js")
+		const delta = Date.now() - before
+		console.log(chalk.green(`✓ compressed in ${readableTime(delta)}`))
+	}
 }
 
 async function watch() {
