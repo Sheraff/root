681f09aa62189b7af86786d21555cb2ef22d27ba
Sheraff <fpellet@ensc.fr>
2024-02-07 20:52:06 +0100

    trying stdout parsing for vitest too

---
+++ .github/test-stdin.ts
@@ -0,0 +1,29 @@
+import * as readline from "node:readline"
+
+const LINE = /^::error/
+const BODY = /^::error\s(.*)::(.*)$/
+const step = process.argv[2]
+
+void (async function () {
+	for await (const line of readline.createInterface({ input: process.stdin })) {
+		const isErrorLine = line.match(LINE)
+		if (!isErrorLine) {
+			console.log(line)
+			continue
+		}
+		const match = line.match(BODY)
+		if (!match) continue
+		const [_, body, message] = match
+		const p: { title: string; file: string; line: string; col: string } = Object.fromEntries(
+			body.split(",").map((part) => {
+				const pair = part.split("=")
+				pair[1] = decodeURIComponent(pair[1])
+				return pair
+			})
+		)
+
+		console.log(
+			`::error file=${p.file},line=${p.line},col=${p.col},title=${step} > ${p.title}::${p.title}\n${message}`
+		)
+	}
+})()
