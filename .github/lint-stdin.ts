3848e26ecb795c82d7e129b25c3fd29a7b501d74
Sheraff <fpellet@ensc.fr>
2024-02-07 02:03:01 +0100

    add eslint parser

---
+++ .github/lint-stdin.ts
@@ -0,0 +1,29 @@
+const readline = require("node:readline")
+
+const CLEAN = /(?:\x1B\[([0-9;]+)m)?/g
+const START = /^[^\s]+\s(\/[^\s]+)$/gm
+const LINE = /^[^\s]+\s+([\d]+):([\d]+)\s+([a-z]+)\s+(.*)\s\s(.*)$/gm
+const END = /^[^\s]+\s$/gm
+
+void (async function () {
+	let file = ""
+	for await (const line of readline.createInterface({ input: process.stdin })) {
+		console.log(line)
+		const clean = line.replace(CLEAN, "")
+		if (!file) {
+			const match = START.exec(clean)
+			if (match) {
+				file = match[1]
+			}
+			continue
+		}
+		if (clean.match(END)) {
+			file = ""
+			continue
+		}
+		const match = LINE.exec(clean)
+		if (!match) continue
+		const [_, l, col, severity, message, rule] = match
+		console.log(`::${severity} file=${file},line=${l},col=${col}::${message} (${rule})`)
+	}
+})()
