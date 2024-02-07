a8c2ba2501ed6b1e1708aaa9e56ab8173f0f414e
Sheraff <fpellet@ensc.fr>
2024-02-07 02:22:00 +0100

    fix parser regex

--- .github/format-stdin.ts
+++ .github/format-stdin.ts
@@ -2,7 +2,7 @@ void (async function () {
 	const readline = require("node:readline")
 
 	const CLEAN = /(?:\x1B\[([0-9;]+)m)?/g
-	const MATCHER = /^[^\s]+\s\[([a-z]+)\]\s(.*)$/gm
+	const MATCHER = /^[^\s]+\s\[([a-z]+)\]\s([^\s]*)$/gm
 	const message = "Code style issues found in this file. Run Prettier to fix."
 
 	for await (const line of readline.createInterface({ input: process.stdin })) {
