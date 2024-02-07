8e395f0cbdde301a8e57815e5b396db0e30acfa2
Sheraff <fpellet@ensc.fr>
2024-02-07 01:47:37 +0100

    try spell stdout parser

---
+++ .github/spell-stdin.ts
@@ -0,0 +1,17 @@
+const readline = require("node:readline")
+
+const CLEAN = /(?:\x1B\[([0-9;]+)m)?/g
+const MATCHER = /\s\.\/([^:]+):([\d]+):([\d]+) - ([^(]+)\s\(([^)]+)\)$/gm
+
+void (async function () {
+	for await (const line of readline.createInterface({ input: process.stdin })) {
+		console.log(line)
+		const clean = line.replace(CLEAN, "")
+		while (true) {
+			const match = TS_MATCHER.exec(clean)
+			if (!match) break
+			const [_, file, l, col, message, word] = match
+			console.log(`::error file=${file},line=${l},col=${col}::${message} (${word})`)
+		}
+	}
+})()
