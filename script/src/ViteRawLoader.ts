6d4838b0324c60505c5ed67d731d5759fabc5625
Sheraff <fpellet@ensc.fr>
2024-02-09 02:15:28 +0100

    restore, why no working

--- script/src/ViteRawLoader.ts
+++ script/src/ViteRawLoader.ts
@@ -1,4 +1,4 @@
-// import { readFile } from "node:fs/promises"
+import { readFile } from "node:fs/promises"
 import { extname } from "node:path"
 import { type Plugin } from "vite"
 
@@ -8,8 +8,7 @@ export function ViteRawLoader(): Plugin {
 		load(url) {
 			const extension = extname(url)
 			if (extension === ".sql" || extension === ".txt") {
-				return `export default ""`
-				// return readFile(url, "utf8").then((source) => `export default \`${source}\``)
+				return readFile(url, "utf8").then((source) => `export default \`${source}\``)
 			}
 		},
 		// handleHotUpdate({ file, server }) {
