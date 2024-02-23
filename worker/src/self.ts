b7ab3b32aa3b489ee42d1c75b3018bec0ad3edc3
Sheraff <fpellet@ensc.fr>
2024-02-23 20:53:45 +0100

    add some SW types

--- worker/src/self.ts
+++ worker/src/self.ts
@@ -1,9 +1,28 @@
+interface SyncManager {
+	getTags(): Promise<string[]>
+	register(tag: string): Promise<void>
+}
+
 declare global {
 	interface WorkerNavigator {
 		connection?: {
 			downlink: number
 			rtt: number
 		}
+	}
+
+	// See https://github.com/GoogleChrome/workbox/issues/2946
+	interface ServiceWorkerRegistration {
+		readonly sync: SyncManager
+	}
+
+	interface SyncEvent extends ExtendableEvent {
+		readonly lastChance: boolean
+		readonly tag: string
+	}
+
+	interface ServiceWorkerGlobalScopeEventMap {
+		sync: SyncEvent
 	}
 }
 
