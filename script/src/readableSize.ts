17d65802d61d6152e76d2d6c668d02cd9d8bf2da
Sheraff <fpellet@ensc.fr>
2024-04-06 20:08:27 +0200

    upgrade everything

--- script/src/readableSize.ts
+++ script/src/readableSize.ts
@@ -1,6 +1,5 @@
 export function readableSize(size: number) {
-	const i = Math.floor(Math.log(size) / Math.log(1024))
-	return (
-		(size / Math.pow(1024, i)).toFixed(i === 0 ? 0 : 2) + " " + ["B", "kB", "MB", "GB", "TB"][i]
-	)
+	const i = Math.min(4, Math.floor(Math.log(size) / Math.log(1024)))
+	const unit = ["B", "kB", "MB", "GB", "TB"][i]!
+	return (size / Math.pow(1024, i)).toFixed(i === 0 ? 0 : 2) + " " + unit
 }
